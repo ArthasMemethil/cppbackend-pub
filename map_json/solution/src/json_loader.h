954c51b69727545991a71348cf28396466f71ea6
ArthasMemethil <102616410+ArthasMemethil@users.noreply.github.com>
2025-09-20 02:12:31 +0300

    Add files via upload

---
+++ map_json/solution/src/json_loader.h
@@ -0,0 +1,34 @@
+#pragma once
+
+
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+#include <boost/json/value.hpp>
+#include <boost/json/object.hpp>
+#include <boost/json.hpp>
+#include <boost/json/string.hpp>
+#include <boost/json/array.hpp>
+
+#include "model.h"
+
+
+
+
+namespace json_loader {
+
+std::string ReadJSONFromPath(const std::filesystem::path& json_path);
+
+std::string ReadJSONFromStream(std::stringstream& input);
+
+void AddingRoadsToMap(const boost::json::array& roads, model::Map& map);
+
+void AddingBuildingsToMap(const boost::json::array& buildings, model::Map& map);
+
+void AddingOfficesToMap(const boost::json::array& offices, model::Map& map);
+
+model::Game LoadGame(const std::filesystem::path& json_path);
+
+}  // namespace json_loader
